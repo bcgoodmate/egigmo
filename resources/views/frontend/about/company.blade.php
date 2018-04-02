ce71bc4ff93354dfa03b2420e41289fff6d05ea6
root <root@egigmo.com>
2018-04-02 03:11:55 +0000

    forever BC initial commit

---
+++ resources/views/frontend/about/company.blade.php
@@ -0,0 +1,9 @@
+@extends('frontend._templates.app')
+
+@section('content')
+	Insert your code herexxxx..
+@endsection@extends('frontend._templates.app')
+
+@section('content')
+	Insert your code herexxxx..
+@endsection
